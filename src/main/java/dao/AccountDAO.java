bf4d1d2e39f1be09ab98db415770d919e0e0858e
Tomoki Yokoyama <89618972+tomoki2002165@users.noreply.github.com>
2021-10-30 03:29:46 +0900

    Update AccountDAO.java

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -12,7 +12,7 @@ import model.Account;
 public class AccountDAO {
 	
 	private final String DRIVER_NAME = "org.h2.Driver";
-	private final String JDBC_URL = "jdbc:h2:~/tomatanToDo";
+	private final String JDBC_URL = "jdbc:h2:~/tomokiToDo";
 	private final String DB_USER = "sa";
 	private final String DB_PASS = "";
 	
